2a3c4f589c985a57b98d32f5191a922bcc2a2dd7
Ron San Jose <ronsj1@gmail.com>
2021-08-03 21:23:41 -0700

    install supabase-js

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-const action = async (context) => {
-  // Do something
+const fs = require('fs')
+const { createClient } = require('@supabase/supabase-js')
 
-  context.notify('Notify about something')
+const contentTypes = new Map([
+  ['.gif', 'image/gif'],
+  ['.mp4', 'video/mp4'],
+  ['.webm', 'video/webm'],
+  ['.apng', 'image/apng'],
+])
+
+// Kap docs: https://github.com/wulkano/Kap/blob/main/docs/plugins.md#action
+const action = async (context) => {
+  context.setProgress('Uploading...')
+  // Connect to Supabase
+  const supabase = createClient(
+    context.config.get('url'),
+    context.config.get('apiKey')
+  )
+
+  const filePath = await context.filePath()
+  const fileName = context.defaultFileName
+  const fileBody = fs.createReadStream(filePath)
+  const contentType =
+    contentTypes.get('.' + context.format) || 'application/octet-stream'
+
+  try {
+    let { error: uploadError, data } = await supabase.storage
+      .from(context.config.get('bucketName'))
+      .upload(fileName, fileBody, {
+        contentType,
+      })
+
+    if (uploadError) {
+      throw uploadError
+    }
+
+    const fileUrl = `${context.config.get('url')}/storage/v1/object/public/${
+      data.Key
+    }`
+      .replace('.co/', '.in/')
+      .trim()
+
+    context.copyToClipboard(fileUrl)
+    context.notify(
+      `URL to the ${context.prettyFormat} has been copied to the clipboard`
+    )
+  } catch (error) {
+    context.notify(error.message)
+  }
 }
 
+// Kap docs: https://github.com/wulkano/Kap/blob/main/docs/plugins.md#config
 const config = {
   url: {
     title: 'Supabase URL',
@@ -13,6 +59,12 @@ const config = {
   },
   apiKey: {
     title: 'Supabase API Key',
+    type: 'string',
+    default: '',
+    required: true,
+  },
+  bucketName: {
+    title: 'Supabase Storage Bucket Name',
     type: 'string',
     default: '',
     required: true,
